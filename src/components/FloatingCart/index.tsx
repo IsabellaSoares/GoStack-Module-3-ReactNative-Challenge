26a1d3e25999f90eba7c345e65d0ef5151f96315
IsabellaSoares <isabella.soares.28@gmail.com>
2021-03-12 17:17:16 -0300

    finish floating cart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const result = products.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.price * currentValue.quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(result);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQtd = products.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.quantity;
+    }, 0);
 
-    return 0;
+    return totalQtd;
   }, [products]);
 
   return (
