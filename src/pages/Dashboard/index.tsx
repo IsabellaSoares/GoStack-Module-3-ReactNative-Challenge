de8467a0df38be1427c038185e7aa3cfa675eb52
IsabellaSoares <isabella.soares.28@gmail.com>
2021-03-12 17:16:50 -0300

    show products in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,16 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      api.get('/products').then(res => {
+        setProducts(res.data);
+      });
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
